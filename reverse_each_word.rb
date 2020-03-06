5b5e5757e89f8d1fe0c27c3fdf36188eaa58639e
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-06 19:07:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,24 @@
+#sentence = "Hello there, and how are you?"
+
+def reverse_each_word(sentence)
+  reverse_sent = sentence.reverse
+  
+  reverse_sent.split.reverse.join(" ")
+end
+
+# def reverse_each_word(sentence)
+# new_sentence = []
+#   sentence.split.each do |word|
+#   new_sentence << word.reverse
+#   end
+#   new_sentence.join(" ")
+# end
+
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
+    word.reverse
+  end.join(" ")
+end
+
+
+
