41b8e0706c5e5dcddb5902e7cdc1fcbb4d9c399c
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-09 02:34:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -20,5 +20,11 @@ def reverse_each_word(sentence)
   end.join(" ")
 end
 
+# def reverse_each_word(sentence)
+#   new_sentence = sentence.split.collect do |word|
+#     word.reverse
+#   end
+#   new_sentence.join(" ")
+# end
 
 
